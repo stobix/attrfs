f771b36f371c6f4f0634d49d21c275622ba3ccf0
Joel Ericson <kasettbok@gmail.com>
2010-02-01 17:17:33 +0100

    getxattr

--- priv/attr_server.c
+++ priv/attr_server.c
@@ -1,27 +1,35 @@
 #include<stdio.h>
 #include<string.h>
+#include<sys/types.h>
+#include<attr/xattr.h>
 #include<attr/attributes.h>
+#include<errno.h>
+
 main(){
-    char path[1024],attr[256],command[1289]="attr -g ",output[1024]; /* Should be enough for anyone! */
+    char path[1024],attr[256],val[256],command[1289]="attr -g ",output[1024]; /* Should be enough for anyone! */
     FILE* fifo;
     for(;;){
         printf("Input: ");
         scanf("%s",command);
         if(strcmp("exit",command)){
             if(!strcmp("fil",command)){
+                int length=255;
                 scanf("%s %s",path,attr);
-                strcpy(command,"attr -g");
-                strcat(command,attr);
-                strcat(command," ");
-                strcat(command,path);
-                printf("%s\n",command);
-                if( (fifo=popen(command,"er"))!=NULL){
-                    fscanf(fifo,"%*[^\n]\n%[^\n]",&output);
-                    printf("Return val:%s\n",output);
-                    pclose(fifo);
-                    output[0]=0;
-                    *command=0;
+                if( getxattr(path,attr,(char*)&val,length) == -1 ){
+                    switch(errno){
+                        case ENOATTR: printf("The attribute in unreachable or does not exist.\n"); break;
+                        case ERANGE: printf("The buffer size is too small\n");
+                                     break;
+                        case ENOTSUP: printf("Extended arguments disabled or not supported on fs! (Or file missing)\n"); break;
+                        default: printf("Unknown error!\n");
+                    }
+
+                } else {
+                    printf("%s has value %s",attr,val);
                 }
+
+
+
             } else {
                 printf("Invalid command!\n");
             }
